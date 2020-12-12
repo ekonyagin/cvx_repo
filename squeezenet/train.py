Accuracy calculation fixed

squeezenet/train.py
@@ -131,7 +131,7 @@ def train_single_model(robust_type, lr, weight_decay, n_epoch, batch_size, train
     best_model.load_state_dict(torch.load("checkpoints/" + model.get_model_name() + ".pth.tar"))
     best_model = best_model.eval()
 
-    test_acc = calculate_accuracy_dataset(best_model, test_dataset, batch_size)
+    test_acc = calculate_accuracy_dataset(best_model, test_dataset, batch_size, DEVICE)
     print("Model trained, test accuracy: " + str(test_acc))
 
 

squeezenet/util.py
@@ -2,19 +2,19 @@ import numpy as np
 import torch
 
 
-def calculate_accuracy(model, batches):
+def calculate_accuracy(model, batches, device):
     n_predictions = 0
     n_correct = 0
 
     with torch.no_grad():
         for X_batch, y_batch in batches:
-            y_pred = torch.argmax(model.forward(X_batch.to(DEVICE)), dim=1).cpu()
+            y_pred = torch.argmax(model.forward(X_batch.to(device)), dim=1).cpu()
             n_correct += (y_pred == y_batch).sum().item()
             n_predictions += y_batch.shape[0]
     
     return n_correct / n_predictions
 
 
-def calculate_accuracy_dataset(model, dataset, batch_size):
+def calculate_accuracy_dataset(model, dataset, batch_size, device):
     loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, num_workers=4, shuffle=False)
-    return calculate_accuracy(model, loader)
+    return calculate_accuracy(model, loader, device)